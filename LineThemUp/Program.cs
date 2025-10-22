289a86cfd5245c02198c40baa25d8a9f274ee5f2
Olle Mattsson <rymdolle@users.noreply.github.com>
2025-10-22 12:17:11 +0200

    LineThemUp

---
+++ LineThemUp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int count = int.Parse(Console.ReadLine()!);
+        string a = Console.ReadLine()!;
+        string b = Console.ReadLine()!;
+        int direction = a.CompareTo(b);
+        for (int i = 2; i < count; i++)
+        {
+            a = b;
+            b = Console.ReadLine()!;
+            if (a.CompareTo(b) != direction)
+            {
+                Console.WriteLine("NEITHER");
+                return;
+            }
+        }
+        Console.WriteLine(direction < 0 ? "INCREASING" : "DECREASING");
+    }
+}
