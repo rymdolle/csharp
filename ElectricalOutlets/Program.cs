636544de67dcca7a28a8e55e10ef860eac164bd9
Olle Mattsson <rymdolle@users.noreply.github.com>
2025-10-23 08:08:52 +0200

    ElectricalOutlets

---
+++ ElectricalOutlets/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int count = int.Parse(Console.ReadLine()!);
+        for (int i = 0; i < count; i++)
+        {
+            string line = Console.ReadLine()!;
+            string[] cords = line.Split(' ')[1..];
+            int ports = 1;
+            foreach (string cord in cords)
+            {
+                ports += int.Parse(cord) - 1;
+            }
+            Console.WriteLine(ports);
+        }
+    }
+}
